c376d4894940d08ecdebe8686380d14332e60bc5
0Armaan025 <armaan33000@gmail.com>
2024-06-09 18:24:41 +0530

    collaborative sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:remote_collab_tool/features/document_editing/document_editing_screen.dart';
 
 import 'package:remote_collab_tool/features/poll_screen/poll_list_screen.dart';
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: PollListScreen(),
+      home: DocumentEditingScreen(),
     );
   }
 }
