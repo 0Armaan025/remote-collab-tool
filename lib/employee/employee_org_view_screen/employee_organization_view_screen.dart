e10de49d85ed733a1e01a871f48110ccd60af84e
0Armaan025 <armaan33000@gmail.com>
2024-06-09 08:21:37 +0530

    feat: tabs thingy added

--- lib/employee/employee_org_view_screen/employee_organization_view_screen.dart
+++ lib/employee/employee_org_view_screen/employee_organization_view_screen.dart
@@ -12,20 +12,104 @@ class EmployeeOrganizationViewScreen extends StatefulWidget {
 }
 
 class _EmployeeOrganizationViewScreenState
-    extends State<EmployeeOrganizationViewScreen> {
+    extends State<EmployeeOrganizationViewScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
         extendBodyBehindAppBar: true,
-        appBar: createAppBar(),
-        bottomNavigationBar: MyBottomNavigationBar(),
-        backgroundColor: Pallete.bgColor,
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: [],
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            "RCT",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
             ),
           ),
-        ));
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 223, 223, 223).withOpacity(0.7),
+                  Color.fromARGB(255, 0, 0, 0).withOpacity(0.2)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+          ),
+          bottom: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey[300],
+            tabs: [
+              Tab(text: "Tasks"),
+              Tab(text: "Members"),
+              Tab(text: "Chat"),
+            ],
+          ),
+        ),
+        bottomNavigationBar: MyBottomNavigationBar(),
+        backgroundColor: Pallete.bgColor,
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            TasksScreen(),
+            MembersScreen(),
+            ChatScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Tasks Screen'));
+  }
+}
+
+class MembersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Members Screen'));
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Chat Screen'));
   }
 }
