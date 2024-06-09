05e92dd013745a7a011f7b13bffc1d1deb595d01
VarunGuptaPy <varun.gupta.py@gmail.com>
2024-06-09 06:45:03 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:remote_collab_tool/features/auth/screens/sign_up_page.dart';
 import 'package:remote_collab_tool/features/home/home_page.dart';
 import 'package:remote_collab_tool/features/onboarding_screens/onboarding_screen.dart';
 import 'package:remote_collab_tool/features/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
